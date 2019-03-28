9a9f3bece33bcdd5245efea3cd88b4af7082edb1
baonguyen96 <baonguyendrump@gmail.com>
2019-03-28 00:19:40 -0500

    Update cryptography

--- src/utility.py
+++ src/utility.py
@@ -1,5 +1,7 @@
-import base64
+from base64 import b64encode, b64decode
 from Crypto.Cipher import AES
+import hashlib
+from Crypto.Util.Padding import pad, unpad
 
 
 def get_substring_between_parentheses(message):
@@ -7,11 +9,17 @@ def get_substring_between_parentheses(message):
 
 
 def encrypt(message, key, iv):
-    aes = AES.new(key, AES.MODE_CBC, iv)
-    return base64.b64encode(iv + aes.encrypt(message))
+    cipher = AES.new(hashlib.md5(key.encode('utf-8')).digest(),
+                     AES.MODE_CBC,
+                     hashlib.md5(iv.encode('utf-8')).digest())
+    ct_bytes = cipher.encrypt(pad(message.encode('utf-8'), AES.block_size))
+    return b64encode(ct_bytes).decode('utf-8')
 
 
 def decrypt(message, key, iv):
-    b64_message = base64.b64decode(message)
-    aes = AES.new(key, AES.MODE_CBC, iv)
-    return aes.decrypt(b64_message)
+    ct = b64decode(message)
+    cipher = AES.new(hashlib.md5(key.encode('utf-8')).digest(),
+                     AES.MODE_CBC,
+                     hashlib.md5(iv.encode('utf-8')).digest())
+    return unpad(cipher.decrypt(ct), AES.block_size).decode('utf-8')
+
